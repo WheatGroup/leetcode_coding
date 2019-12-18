ca7bc8e240e71078931c926359f4a8aabf807bd2
wheatVPN <492847247@qq.com>
2019-12-18 14:28:37 +0800

    实现前缀树

--- 208/main.py
+++ 208/main.py
@@ -15,23 +15,68 @@ trie.search("app");     // 返回 true
 '''
 class TrieNode(object):
     def __init__(self):
-        self.links = []
+        # 此处不能 self.links = []
+        # 因为这样要是从links中按照下标选择字符的时候 会报错
+        self.links = [None] * 26
         self.is_end = False
 
-    def put(self, ch):
+    def put(self, ch, node):
+        self.links[ord(ch) - ord('a')] = node
 
-    def get(self, ch, node):
+    def get(self, ch):
+        return self.links[ord(ch) - ord('a')]
 
-    def
+    def containKeys(self, ch):
+        # 判断这个结点是否含有某个字符
+        return self.links[ord(ch) - ord('a')] != None
 
+    def setEnd(self):
+        self.is_end = True
 
 
 class Trie(object):
     def __init__(self):
-        self.root = {}
-        self.
+        self.root = TrieNode()
 
-    def insert(self, word: str) -> None:
 
-    def search(self, word: str) -> None:
+    def insert(self, words):
+        node = self.root
+        for ch in words:
+            if not node.containKeys(ch):
+                node.put(ch, TrieNode())
+            node = node.get(ch)
+        node.setEnd()
+
+
+    def searchPrefix(self, words):
+        # 搜索单词的前缀 返回值是None 或者一个node
+        # 只有node的is_end是True的时候 才代表有完整的单词 否则只是前缀
+        node = self.root
+        for ch in words:
+            if node.containKeys(ch):
+                node = node.get(ch)
+            else:
+                return None
+        return node
+
+
+    def search(self, words):
+        node = self.searchPrefix(words)
+        return node is not None and node.is_end
+
+
+    def startsWith(self, words):
+        node = self.searchPrefix(words)
+        return node is not None
+
+
+trie = Trie()
+
+trie.insert("apple")
+result1 = trie.search("apple")
+result2 = trie.search("app")
+result3 = trie.startsWith("app")
+trie.insert("app")
+result4 = trie.search("app")
+print(result1, result2, result3, result4)
 
